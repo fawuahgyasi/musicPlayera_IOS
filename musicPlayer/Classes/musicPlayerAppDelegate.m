164b3e8805a6b52a5d6a7137cd950e23dc5987dd
Frederick Awuah-Gyasi <fawuahgyasi@yahoo.com>
2013-06-07 05:06:34 -0400

    Update musicPlayerAppDelegate.m

--- musicPlayer/Classes/musicPlayerAppDelegate.m
+++ musicPlayer/Classes/musicPlayerAppDelegate.m
@@ -2,8 +2,8 @@
 //  musicPlayerAppDelegate.m
 //  musicPlayer
 //
-//  Created by Justin Watkins on 2/16/12.
-//  Copyright 2012 North Carolina State University. All rights reserved.
+//  Created by Frederick Awuah-Gyasi on 2/16/12.
+//  Copyright 2012 Wake Forest University. All rights reserved.
 //
 
 #import "musicPlayerAppDelegate.h"
